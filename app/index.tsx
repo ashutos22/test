e83f47e93e4f5955ecef6ab609b837f9df736077
ashutos22 <ashutosarun1995@gmail.com.com>
2025-01-12 15:46:33 +0530

    command

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>{process.env.EXPO_PUBLIC_ENV}</Text>
     </View>
   );
 }
